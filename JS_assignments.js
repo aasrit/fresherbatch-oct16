7bdc281e8c2b75286290051fcdf6b6562e718fe4
aasrit <aasritha.mandapaka@capgemini.com>
2022-10-15 12:23:21 +0530

    new file

---
+++ JS_assignments.js
@@ -0,0 +1,9 @@
+<html>
+<script>
+var num1,num2,sum
+num1=prompt("enter first number")
+num2=prompt("enter second number")
+sum=parseInt(num1)+parseInt(num2)//5+
+alert("sum="+sum)
+</script>
+</html>
